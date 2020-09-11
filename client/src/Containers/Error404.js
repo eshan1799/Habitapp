9b2515337b974f6bee531f38b04c180009f47714
eshan1799 <eshan1799@gmail.com>
2020-09-11 13:27:25 +0100

    add styling to error page

--- client/src/Containers/Error404.js
+++ client/src/Containers/Error404.js
@@ -3,11 +3,12 @@ import { NavLink, withRouter } from 'react-router-dom'
 const Error404 = props => {
     return (
         <div>
-        <h2>Oops!</h2>
+        <br />
+        <label>Oops!</label>
         <section className="goToHome">
-            <p>We don't have a page called { props.location.pathname }!</p>
+            <div><p>We don't have a page called { props.location.pathname }!</p></div>
             <NavLink to="/">
-                <button>Go to homepage</button>
+                <button className='submitButtons'>Go to homepage</button>
             </NavLink>
         </section>
         </div>
